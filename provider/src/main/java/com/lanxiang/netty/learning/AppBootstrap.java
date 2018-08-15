15fa0147e6ce6296499ca6fcde00a6193edf7d54
兰兢 <lanjing02@maoyan.com>
2018-08-15 16:49:32 +0800

    complete

--- provider/src/main/java/com/lanxiang/netty/learning/AppBootstrap.java
+++ provider/src/main/java/com/lanxiang/netty/learning/AppBootstrap.java
@@ -2,6 +2,7 @@ package com.lanxiang.netty.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.PropertySource;
 /**
  * 应用启动类
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @PropertySource({
         "classpath:config/common.properties"
 })
@@ -22,6 +23,4 @@ public class AppBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(AppBootstrap.class, args);
     }
-
-
 }
